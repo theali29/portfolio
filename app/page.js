7de3e272383471088e93b00d33c1141969f63c08
theali29 <muhammad.786685@gmail.com>
2024-11-04 17:56:26 +0500

    Updated changes

--- app/page.js
+++ app/page.js
@@ -38,11 +38,11 @@ export default function Home() {
         <section className=' min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
             <h1 className='text-xl playfair_display underline'>Mohammad A. </h1>
-            <ul className='flex items-center'>
+            {/* <ul className='flex items-center'>
               {/* <li>
                 <DarkModeToggle onToggle={handleDarkModeToggle} />
               </li> */}
-              <li>
+            {/* <li>
                 <a
                   className='card big-gradient-to-r bg-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'
                   href='/Mohammad Ali - ASE.pdf'
@@ -51,7 +51,7 @@ export default function Home() {
                   Resume
                 </a>
               </li>
-            </ul>
+            </ul> */}
           </nav>
           <div className='relative mx-auto w-40 h-40 overflow-hidden rounded-full bg-gradient-to-b'>
             <Image
@@ -195,6 +195,7 @@ export default function Home() {
                 <li>MongoDB</li>
                 <li>MySQL</li>
                 <li>PostgreSQL</li>
+                <li>PHP</li>
               </ul>
             </div>
             <div className='shadow-lg p-10 rounded-xl my-10'>
