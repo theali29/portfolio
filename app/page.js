0edeab3b75c126fd88e390a56a0622d3c1affe83
theali29 <muhammad.786685@gmail.com>
2024-12-05 18:17:57 +0500

    Some changes

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Head from 'next/head'
 import deved from '../public/IMG_1126.jpg'
 import detector from '../public/detector.png'
+import forest from '../public/forestio.png'
 import ContactForm from '@/components/contactform'
 import { BsFillMoonStarsFill } from 'react-icons/bs'
 import {
@@ -14,13 +15,16 @@ import frontend from '../public/front-end.png'
 import backend from '../public/backend.png'
 import mobileapp from '../public/mobileapp.png'
 import web1 from '../public/web1.png'
+import weather from '../public/weatherapp.png'
 // import web2 from '../public/web2.png'
 // import web3 from '../public/web3.png'
 import web4 from '../public/web4.png'
 import web5 from '../public/web5.png'
 import web6 from '../public/web6.png'
 import gym from '../public/gym1.png'
-import tabs from '../public/image1.png'
+import tabs from '../public/tabsform.png'
+import chic from '../public/chicdreams.png'
+
 import education from '../public/education1.png'
 import login from '../public/loginpage.png'
 // import web6 from '../public/web6.png'
@@ -96,7 +100,7 @@ export default function Home() {
                   </h3>
                   <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto'>
                     Hello! I am a passionate and driven full-stack developer
-                    ready to bring innovative solutions to your startup or
+                    from Pakistan ready to bring innovative solutions to your
                     business. With a solid foundation in both web and mobile
                     development, I am committed to delivering high-quality,
                     tailored solutions that meet your unique needs.
@@ -273,11 +277,7 @@ export default function Home() {
             <div>
               <h3 className=' h3mktg text-3xl py-1'>Projects</h3>
               <p className='text-md py-2 leading-8 text-gray-800 text-xl font-serif'>
-                I am excited to share some of the projects I have worked on
-                while learning and developing my skills. Each of these projects
-                represents a unique challenge and a valuable learning
-                experience. Feel free to explore them and see the progress I
-                have made along the way!
+                Some of the recent projects
               </p>
             </div>
             <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-x-6 py-10 lg:flex-row lg:flex-wrap'>
@@ -371,6 +371,97 @@ export default function Home() {
                   </div>
                 </div>
               </div>
+              <div className='flex-col shadow-lg shadow-amber-800 rounded-xl mt-0 mb-8 ml-0 mr-4 mx-0 basis-1/3 flex-1 w-full h-fit'>
+                <Image
+                  src={forest}
+                  className='rounded-lg object-cover'
+                  style={{
+                    width: '384px !important',
+                    height: '192px !important',
+                  }}
+                  layout='responsive'
+                  alt='Forest.io'
+                />
+                <div className='py-8 px-6'>
+                  <h3 className='mt-0 text-3xl playfair_display font-medium'>
+                    Forest.io
+                  </h3>
+                  <div className='my-3 text-base leading-[1.2] tracking-lighter'>
+                    Forest.io is a clean technology company that utilizes
+                    artificial intelligence to safeguard the environment by
+                    halting the voluntary carbon market through transparent
+                    offset solutions.
+                  </div>
+                  <div className='mt-auto self-end'>
+                    <a
+                      href='https://forest.io'
+                      className='items-center flex p-1 w-fit font-bold text-amber-950'
+                    >
+                      <svg
+                        stroke='currentColor'
+                        fill='currentColor'
+                        stroke-width='0'
+                        viewBox='0 0 16 16'
+                        height='20'
+                        width='20'
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='mr-1'
+                      >
+                        <path d='M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z'></path>
+                        <path d='M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z'></path>
+                      </svg>
+                      Website
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className='flex-col shadow-lg shadow-amber-800 rounded-xl mt-0 mb-8 ml-0 mr-4 mx-0 basis-1/3 flex-1 w-full h-fit'>
+                <Image
+                  src={chic}
+                  className='rounded-lg object-cover'
+                  style={{
+                    width: '384px !important',
+                    height: '192px !important',
+                  }}
+                  layout='responsive'
+                  alt='Chic Dreams'
+                />
+                <div className='py-8 px-6'>
+                  <h3 className='mt-0 text-3xl playfair_display font-medium'>
+                    Chic Dreams
+                  </h3>
+                  <div className='my-3 text-base leading-[1.2] tracking-lighter'>
+                    Contributed to Chic Dreams, a family-owned online furniture
+                    store, by enhancing brand identity and customer experience.
+                    Focused on curating stylish bed frames and quality
+                    mattresses, while implementing strategies for exceptional
+                    customer service. Developed expertise in product selection
+                    and pricing strategies to drive sales and customer
+                    satisfaction.
+                  </div>
+                  <div className='mt-auto self-end'>
+                    <a
+                      href='https://chicdreams.co.uk'
+                      className='items-center flex p-1 w-fit font-bold text-amber-950'
+                    >
+                      <svg
+                        stroke='currentColor'
+                        fill='currentColor'
+                        stroke-width='0'
+                        viewBox='0 0 16 16'
+                        height='20'
+                        width='20'
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='mr-1'
+                      >
+                        <path d='M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z'></path>
+                        <path d='M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z'></path>
+                      </svg>
+                      Website
+                    </a>
+                  </div>
+                </div>
+              </div>
               <div className='flex-col shadow-lg shadow-amber-950 rounded-xl mt-0 mb-8 ml-0 mr-4 mx-0 basis-1/3 flex-1 w-full h-fit'>
                 <Image
                   src={gym}
@@ -395,7 +486,7 @@ export default function Home() {
                   </div>
                   <div className='mt-auto self-end'>
                     <a
-                      href='https://fitness-landing-page1.netlify.app/'
+                      href='https://github.com/theali29/gym_landing_page'
                       className='items-center flex p-1 w-fit font-bold text-amber-950'
                     >
                       <svg
@@ -474,11 +565,11 @@ export default function Home() {
                     Aimm
                   </h3>
                   <div className='my-3 text-base leading-[1.2] tracking-lighter'>
-                    It is a next-generation form-building SaaS platform designed
-                    to revolutionize how forms and surveys are created and
-                    experienced. Tabsform offers features like real-time
-                    input-driven adaptation, AI-based personalization,
-                    gamification, and deep analytics integration.
+                    It is a UI design system that empowers the creation of
+                    exceptional user experiences. With prebuilt templates and a
+                    simple drag-and-drop interface, designers can effortlessly
+                    integrate elements into their projects and complete designs
+                    in minutes.
                   </div>
                   <div className='mt-auto self-end'>
                     <a
@@ -505,27 +596,29 @@ export default function Home() {
               </div>
               <div className='flex-col shadow-lg shadow-amber-800 rounded-xl mt-0 mb-8 ml-0 mr-4 mx-0 basis-1/3 flex-1 w-full h-fit'>
                 <Image
-                  src={web6}
+                  src={weather}
                   className='rounded-lg object-cover'
                   width={'100%'}
                   height={'100%'}
                   layout='responsive'
-                  alt='Nunito'
+                  alt='Weather App'
                 />
                 <div className='py-8 px-6'>
                   <h3 className='mt-0 text-3xl playfair_display font-medium'>
-                    Nunito
+                    Weather App
                   </h3>
                   <div className='my-3 text-base leading-[1.2] tracking-lighter'>
-                    It is a next-generation form-building SaaS platform designed
-                    to revolutionize how forms and surveys are created and
-                    experienced. Tabsform offers features like real-time
-                    input-driven adaptation, AI-based personalization,
-                    gamification, and deep analytics integration.
+                    It provides users with real-time weather information and
+                    detailed forecasts. It also features secure user
+                    authentication through sign-in and sign-up pages, utilizing
+                    Firebase for data protection. By integrating the OpenWeather
+                    API, the app delivers up-to-date weather data, ensuring
+                    users can easily access current conditions and forecasts for
+                    their locations.
                   </div>
                   <div className='mt-auto self-end'>
                     <a
-                      href='https://tabsform.netlify.app'
+                      href='https://github.com/theali29/weatherapp'
                       className='items-center flex p-1 w-fit font-bold text-amber-950'
                     >
                       <svg
